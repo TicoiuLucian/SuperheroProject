7f9f3da6b1a8ab6b018af3cd61cf5000cbf065da
Szekely Sebastian <ssebastian9928@gamil.com>
2022-12-22 18:48:27 +0200

    Superhero entity

--- src/main/java/ro/itschool/entity/Superhero.java
+++ src/main/java/ro/itschool/entity/Superhero.java
@@ -1,4 +1,29 @@
 package ro.itschool.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
 public class Superhero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String realName;
+
+
+    @ManyToMany
+    private List<Weakness>weaknesses;
+
+    @ManyToMany
+    private List<Power> powers;
+
+
 }
