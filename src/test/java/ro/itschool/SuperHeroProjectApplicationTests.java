b4bad272e89764eff82f51826610740676997a9f
Ticoiu Lucian <lucian.ticoiu.ext@alten.com>
2022-12-22 18:34:25 +0200

    Initial commit

---
+++ src/test/java/ro/itschool/SuperHeroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.itschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
