05a1df72bc90799629255abfe5fb694833abc2e5
Dani Ciucur <dani_ciucur92@yahoo.com>
2023-01-02 14:07:57 +0200

    CRUD operations

--- src/main/java/ro/itschool/controller/SuperheroController.java
+++ src/main/java/ro/itschool/controller/SuperheroController.java
@@ -1,13 +1,12 @@
 package ro.itschool.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ro.itschool.entity.Superhero;
 import ro.itschool.repository.SuperheroRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/superhero")
@@ -17,9 +16,39 @@ public class SuperheroController {
     @Autowired
     private SuperheroRepository superheroRepository;
 
-    @GetMapping
+    @GetMapping(value = "/all")
     private List<Superhero> getAllSuperheroes() {
         return superheroRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public void getSuperHeroById (@PathVariable Long id){
+        superheroRepository.findById(id);
+    }
+
+    @GetMapping(value = "/name")
+    public void getSuperheroesByName(@RequestParam Superhero superhero){
+        superheroRepository.findByName(superhero.getName());
+    }
+
+    @GetMapping(value = "/realname")
+    public void getAllSuperheroesByRealName(@RequestParam Superhero superhero){
+        superheroRepository.findByRealName(superhero.getRealName());
+    }
+
+    @PostMapping
+    private void saveSuperhero(@RequestBody Superhero superhero) {
+        superheroRepository.save(superhero);
+    }
+
+    @DeleteMapping(value = "/{id)")
+    private void deleteSuperhero(@RequestParam long id) {
+        superheroRepository.deleteById(id);
+    }
+
+    @PutMapping(value = "/update")
+    private void updateSuperhero(@RequestBody Superhero newSuperhero) {
+        superheroRepository.save(newSuperhero);
     }
 
 }
--- src/main/java/ro/itschool/repository/SuperheroRepository.java
+++ src/main/java/ro/itschool/repository/SuperheroRepository.java
@@ -3,7 +3,12 @@ package ro.itschool.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.itschool.entity.Superhero;
 
+import java.util.List;
+
 public interface SuperheroRepository extends JpaRepository<Superhero, Long> {
 
 
+    void findByName(String name);
+
+    void findByRealName(String realName);
 }
